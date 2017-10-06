bc8324bf42991070fa52647cd999adba848d9129
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2017-10-06 12:02:28 +0200

    Add functions to remove mentions and hashtags

--- fetch_tweets.py
+++ fetch_tweets.py
@@ -1,16 +1,37 @@
+import re
 import tweepy
 from config import keys 
 
-auth = tweepy.OAuthHandler(keys['CONSUMER_KEY'], keys['CONSUMER_SECRET'])
-auth.set_access_token(keys['OAUTH_TOKEN'], keys['OAUTH_TOKEN_SECRET'])
-api = tweepy.API(auth)
+def remove_hashtags(tweet):
+    template = r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)" 
+    tweet = re.sub(template, '', tweet)
+    return tweet
 
-with open('data/ids', 'r') as ids, open('data/tweets', 'w') as tweets:
-    for line in ids:
-        id, label = line.split(',')
-        try:
-            text = api.get_status(id).text
-            text = text.lower()
-            tweets.write("{} {}".format(text, label))
-        except TweepError:
-            continue
+def remove_mentions(tweet):
+    template = r'(?:@[\w_]+)'
+    tweet = re.sub(template, '', tweet)
+    return tweet
+
+def remove_punctuation(tweet):
+    pass
+
+def clean(tweet):
+    tweet = remove_hashtags(tweet)
+    tweet = remove_mentions(tweet)
+    return tweet.lower()
+
+if __name__ == '__main__':
+    auth = tweepy.OAuthHandler(keys['CONSUMER_KEY'], keys['CONSUMER_SECRET'])
+    auth.set_access_token(keys['OAUTH_TOKEN'], keys['OAUTH_TOKEN_SECRET'])
+    api = tweepy.API(auth)
+
+    with open('data/ids', 'r') as ids, open('data/tweets', 'w') as tweets:
+        for line in ids:
+            id, label = line.split(',')
+            try:
+                tweet = api.get_status(id)
+                text = clean(tweet.text)
+                tweets.write("{} {}".format(tweet.text, label))
+                tweets.write("{} {}".format(text, label))
+            except tweepy.TweepError:
+                continue
