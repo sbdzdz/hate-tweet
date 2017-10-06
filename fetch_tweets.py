403ff9221888e88b39a2422ce98ab9fa2827781f
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2017-10-06 17:24:22 +0200

    Move tweet processing to a separate file

--- fetch_tweets.py
+++ fetch_tweets.py
@@ -1,38 +1,6 @@
-import re
-import string
 import tweepy
 from config import keys 
-
-def remove_hashtags(tweet):
-    template = r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)" 
-    tweet = re.sub(template, '', tweet)
-    return tweet
-
-def remove_mentions(tweet):
-    template = r'(?:@[\w_]+)'
-    tweet = re.sub(template, '', tweet)
-    return tweet
-
-def remove_punctuation(tweet):
-    translation = str.maketrans('', '', string.punctuation)
-    return tweet.translate(translation)
-
-def remove_urls(tweet):
-    template = r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+'
-    tweet = re.sub(template, '', tweet)
-    return tweet
-
-def contract_whitespace(tweet):
-    tweet = re.sub("\s\s+", " ", tweet.strip())
-    return tweet 
-
-def clean(tweet):
-    tweet = remove_hashtags(tweet)
-    tweet = remove_mentions(tweet)
-    tweet = remove_urls(tweet)
-    tweet = remove_punctuation(tweet)
-    tweet = contract_whitespace(tweet)
-    return tweet.lower()
+from tweet_utils import clean
 
 if __name__ == '__main__':
     auth = tweepy.OAuthHandler(keys['CONSUMER_KEY'], keys['CONSUMER_SECRET'])
---
+++ tweet_utils.py
@@ -0,0 +1,34 @@
+import re
+import string
+
+def remove_hashtags(tweet):
+    template = r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)" 
+    tweet = re.sub(template, '', tweet)
+    return tweet
+
+def remove_mentions(tweet):
+    template = r'(?:@[\w_]+)'
+    tweet = re.sub(template, '', tweet)
+    return tweet
+
+def remove_punctuation(tweet):
+    translation = str.maketrans('', '', string.punctuation)
+    return tweet.translate(translation)
+
+def remove_urls(tweet):
+    template = r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+'
+    tweet = re.sub(template, '', tweet)
+    return tweet
+
+def contract_whitespace(tweet):
+    tweet = re.sub("\s\s+", " ", tweet.strip())
+    return tweet 
+
+def clean(tweet):
+    tweet = remove_hashtags(tweet)
+    tweet = remove_mentions(tweet)
+    tweet = remove_urls(tweet)
+    tweet = remove_punctuation(tweet)
+    tweet = contract_whitespace(tweet)
+    return tweet.lower()
+
