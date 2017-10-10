985b91054a96d9131feb32f3457911cc34f823c7
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2017-10-10 23:05:35 +0200

    Remove unused imports and print statements

--- model.py
+++ model.py
@@ -1,10 +1,7 @@
-#from __future__ import print_function
 import numpy as np
-
 from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
-from keras.datasets import imdb
 from keras.preprocessing.text import one_hot
 
 
@@ -21,33 +18,31 @@ def to_numeric(label):
     mapping = {'__label__none': 0, '__label__offensive': 1, '__label__hate': 2}
     return mapping.get(label, 0)
 
-max_features = 20000
-maxlen = 30 
-vocabulary_size = 20000
-batch_size = 32
+if __name__ == '__main__':
+    max_features = 20000
+    maxlen = 30
+    vocabulary_size = 20000
+    batch_size = 32
+    train = 'data/tweets_davidson_train'
+    test = 'data/tweets_davidson_test'
 
-print('Loading data...')
-x_train, y_train = read_data('data/tweets_davidson_train')
-x_test, y_test = read_data('data/tweets_davidson_test')
-        
-print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
-y_train = np.array(y_train)
-y_test = np.array(y_test)
+    x_train, y_train = read_data(train)
+    x_test, y_test = read_data(test)
 
-model = Sequential()
-model.add(Embedding(max_features, 128, input_length=maxlen))
-model.add(Bidirectional(LSTM(64)))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
+    x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+    x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+    y_train = np.array(y_train)
+    y_test = np.array(y_test)
 
-model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+    model = Sequential()
+    model.add(Embedding(max_features, 128, input_length=maxlen))
+    model.add(Bidirectional(LSTM(64)))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
 
-print('Train...')
-model.fit(x_train, y_train,
-        batch_size=batch_size,
-        epochs=30,
-        validation_data=[x_test, y_test])
+    model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+
+    model.fit(x_train, y_train,
+            batch_size=batch_size,
+            epochs=30,
+            validation_data=[x_test, y_test])
