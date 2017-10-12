06d0be7640db1869d5e2865822a66576a983d836
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2017-10-12 17:51:35 +0200

    Include the train/test split in the data generator

--- model.py
+++ model.py
@@ -1,18 +1,28 @@
 import numpy as np
+import random
 from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
 from keras.models import Sequential
 from keras.preprocessing import sequence
 from keras.preprocessing.text import one_hot
 
 
-def read_data(filename, vocabulary_size=10000):
-    x, y, = [], []
+def read_data(filename, vocabulary_size=10000, split=0.2):
+    split = max(1-split, split)
+    x_train, y_train, x_test, y_test = [], [], [], []
     with open(filename, 'r') as f:
         for line in f:
-            label, *tweet = line.split()
-            x.append(one_hot(' '.join(tweet), vocabulary_size))
-            y.append(to_numeric(label))
-    return (x, y)
+            try:
+                label, tweet = line.split('\t')
+                tweet = tweet.strip()
+            except ValueError:
+                continue
+            if random.random() < split:
+                x_train.append(one_hot(tweet, vocabulary_size))
+                y_train.append(to_numeric(label))
+            else:
+                x_test.append(one_hot(tweet, vocabulary_size))
+                y_test.append(to_numeric(label))
+    return ((x_train, y_train), (x_test, y_test))
 
 def to_numeric(label):
     mapping = {'__label__none': 0, '__label__offensive': 1, '__label__hate': 2}
@@ -23,11 +33,8 @@ if __name__ == '__main__':
     maxlen = 30
     vocabulary_size = 20000
     batch_size = 32
-    train = 'data/tweets_davidson_train'
-    test = 'data/tweets_davidson_test'
-
-    x_train, y_train = read_data(train)
-    x_test, y_test = read_data(test)
+    path = 'data/tweets_davidson'
+    (x_train, y_train), (x_test, y_test) = read_data(path)
 
     x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
     x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
