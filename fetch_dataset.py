b754ab2a94b07ee43de7152f23da526152648f0e
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2017-10-13 14:35:32 +0200

    Refactor the data fetcher

--- fetch_dataset.py
+++ fetch_dataset.py
@@ -1,19 +1,42 @@
 import datadotworld as dw
 import numpy as np
-from tweet_utils import clean
+import os
+import random
+from utils import clean
 
-if __name__ == '__main__':
+def download_dataframe():
     query = """SELECT does_this_tweet_contain_hate_speech, tweet_text
                FROM twitter_hate_speech_classifier_dfe_a845520"""
-    results = dw.query('crowdflower/hate-speech-identification', query)
-    df = results.dataframe
-    df.columns = ['label', 'text']
+    result = dw.query('crowdflower/hate-speech-identification', query)
+    df = result.dataframe
+    return df
 
-    conditions = [
-        (df['label'] == 'The tweet contains hate speech'),
-        (df['label'] == 'The tweet uses offensive language but not hate speech'),
-        (df['label'] == 'The tweet is not offensive')]
-    choices = ['__label__hate', '__label__offensive', '__label__none']
-    df['class'] = np.select(conditions, choices, default='__label__none')
+def transform(df):
+    df.columns = ['label', 'text']
+    df['label'] = df['label'].apply(translate)
     df['text'] = df['text'].apply(clean)
-    df[['class', 'text']].to_csv('data/tweets', sep='\t', index=False, header=False)
+    return df
+
+def translate(label):
+    translation = {
+        'The tweet contains hate speech': '__label__hate',
+        'The tweet uses offensive language but not hate speech': '__label__offensive',
+        'The tweet is not offensive': '__label__none'
+    }
+    return translation[label]
+
+def split(df, treshold=0.8):
+    mask = np.random.rand(len(df)) < treshold
+    train, test = df[mask], df[~mask] 
+    return (train, test) 
+
+if __name__ == '__main__':
+    df = download_dataframe()
+    df = transform(df)
+
+    os.makedirs('data', exist_ok=True)
+    df.to_csv('data/tweets', sep='\t', index=False, header=False)
+
+    train, test = split(df)
+    train.to_csv('data/tweets_train', sep='\t', index=False, header=False)
+    test.to_csv('data/tweets_test', sep='\t', index=False, header=False)
